5ef92b39772f15b080a0e318119adb94f8bda5fd
SeaMoon <seamoon.dev@gmail.com>
2023-02-10 22:46:46 +0900

    init project

---
+++ src/main/java/com/seamoon/pjt/thymeleafpjt/ThymeleafpjtApplication.java
@@ -0,0 +1,13 @@
+package com.seamoon.pjt.thymeleafpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafpjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafpjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seamoon/pjt/thymeleafpjt/ThymeleafpjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seamoon.pjt.thymeleafpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafpjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
